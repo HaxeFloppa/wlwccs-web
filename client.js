87bf427cec62f4e8944a54dbde6d5d0c9a6bc4d4
HaxeFloppa <rafaelandyhailu@gmail.com>
2025-09-27 08:56:49 +0100

    whoops

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // const socket = new WebSocket("ws://5.80.142.132");
 // const WebSocket = require('ws');
 const socket = new WebSocket("wss://5.80.142.132:11110");
-document.getElementById("reqsend").addEventListener("click", reqsend);
+document.getElementById("reqsend").addEventListener("click", sendreq);
 
 socket.onopen = () => {
 	console.log('connected');
